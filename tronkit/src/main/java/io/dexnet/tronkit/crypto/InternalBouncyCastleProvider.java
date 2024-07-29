919a0d8b01184852f960913b287b0a1a4b7a1ae2
Vlad Legkiy <ukadoV@gmail.com>
2024-07-29 23:46:11 +0300

    changed package 2

--- tronkit/src/main/java/io/dexnet/tronkit/crypto/InternalBouncyCastleProvider.java
+++ tronkit/src/main/java/io/dexnet/tronkit/crypto/InternalBouncyCastleProvider.java
@@ -15,8 +15,8 @@ public final class InternalBouncyCastleProvider {
 
             INSTANCE = (p != null) ? p : new BouncyCastleProvider();
 
-            INSTANCE.put("MessageDigest.ETH-KECCAK-256", "io.horizontalsystems.tronkit.crypto.digest.Keccak256");
-            INSTANCE.put("MessageDigest.ETH-KECCAK-512", "io.horizontalsystems.tronkit.crypto.digest.Keccak512");
+            INSTANCE.put("MessageDigest.ETH-KECCAK-256", "io.dexnet.tronkit.crypto.digest.Keccak256");
+            INSTANCE.put("MessageDigest.ETH-KECCAK-512", "io.dexnet.tronkit.crypto.digest.Keccak512");
         }
     }
 
